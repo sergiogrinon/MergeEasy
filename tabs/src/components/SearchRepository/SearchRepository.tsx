3e61611028425a1f03f455cb8d6574cd3c48e05b
Sergio <sergiochico@microsoft.com>
2022-10-07 12:05:45 +0100

    Housekeeping and new comps

--- tabs/src/components/SearchRepository/SearchRepository.tsx
+++ tabs/src/components/SearchRepository/SearchRepository.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 import { AppService } from "../../services/app.service";
+import {
+  provideFluentDesignSystem,
+  fluentTextField,
+  fluentButton,
+} from "@fluentui/web-components";
+import { provideReactWrapper } from "@microsoft/fast-react-wrapper";
+import "./SearchRepository.styles.css";
+
+const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
+
+//Fluent components
+export const FluentTextField = wrap(fluentTextField());
+export const FluentButton = wrap(fluentButton());
 
 function SearchRepository(): JSX.Element {
   const appService = new AppService();
@@ -7,12 +20,16 @@ function SearchRepository(): JSX.Element {
   const fetchPullRequests = async (e: any) => {
     const response = await appService.getPullRequests();
     console.log(response);
-  }
+  };
 
   return (
     <>
-      <div>
-        <button onClick={fetchPullRequests}>Click Me</button>
+      <div id="search-box" className="searchBox">
+        <h1>Find all your pull requests</h1>
+        <FluentTextField className="textInput" />
+        <FluentButton appearance="accent" onClick={fetchPullRequests}>
+          Click Me
+        </FluentButton>
       </div>
     </>
   );
